ae35a96d7273545a890f2dd2315fc34af4cb0a5f
sebastienwolf <s.mouret.pro@gmail.com>
2022-09-06 15:00:16 +0200

    HTML CSS

---
+++ script.js
@@ -0,0 +1,28 @@
+popHeader();
+popCards();
+
+function popHeader() { }
+
+function popCards() {
+    const actualite = document.getElementById("actualite");
+
+    for (let boucle = 0; boucle < 3; boucle++) {
+        let card = document.createElement('a');
+        let top = document.createElement('div');
+        let tata = document.createElement('img');
+        let bottom = document.createElement('div');
+        let text = document.createElement('p');
+
+        card.className = 'cards';
+        card.href = "#";
+        tata.src = './img/logoCard.png';
+        text.innerText = "L'info du jour";
+        top.className = "topCard";
+        bottom.className = "bottomCard"
+        top.appendChild(tata);
+        bottom.appendChild(text);
+        card.appendChild(top);
+        card.appendChild(bottom);
+        actualite.appendChild(card);
+    }
+}
